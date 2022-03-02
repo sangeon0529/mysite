408ceaf5a17ac3d59ce4eadb958e3fa9ac884213
sangeonpark <parksang1324@gmail.com>
2022-03-02 15:54:00 +0900

    3/2 guestbook final

---
+++ mysite03/src/main/java/com/poscoict/mysite/controller/api/GuestbookController.java
@@ -0,0 +1,10 @@
+package com.poscoict.mysite.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/guestbookApiController")
+@RequestMapping("/guestbook/api")
+public class GuestbookController {
+	
+}
